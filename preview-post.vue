2c5b70ba94aa9b950b4a4250cc81c0e604308375
AC <chetmqz@gmail.com>
2023-06-03 11:41:25 +0330

    Create preview-post.vue

---
+++ preview-post.vue
@@ -0,0 +1,11 @@
+   <template>
+     <div>
+       <h1>All Posts</h1>
+       <ul>
+         <li v-for="post in posts" :key="post.id">
+           <nuxt-link :to="`/${post.slug}`">{{ post.title }}</nuxt-link>
+         </li>
+       </ul>
+     </div>
+   </template>
+//---------------------
